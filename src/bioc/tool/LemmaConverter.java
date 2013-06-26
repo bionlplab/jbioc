57129299d2186c897a8b5bf0021d86a269a2c6ed
Don Comeau <comeau@ncbi.nlm.nih.gov>
2013-06-26 16:05:42 -0400

    added the corresponding java doc to lemmaConverter class

--- src/bioc/tool/LemmaConverter.java
+++ src/bioc/tool/LemmaConverter.java
@@ -9,50 +9,60 @@ import bioc.util.CopyConverter;
 import edu.ucdenver.ccp.nlp.biolemmatizer.BioLemmatizer;
 
 /**
- * BioC wrapper
+ * BioC wrapper for the BioLemmatizer (http://biolemmatizer.sourceforge.net)
+ * reads the BioC input xml file, generates lemmas for the corresponding 
+ * token and its POS tag, inserts the resulting lemmas into the 
+ * corresponding annotation of the xml file following the BioC-compliant
+ * format, and finally outputs the updated BioC xml file.
  */
 public class LemmaConverter extends CopyConverter{
-  /** lemma cache */
-  private Map<String, String> lemmaMap;
+  /** lemma lookup cache */
+  private Map<String, String> lemmaLookupMap;
   
   /** load BioLemmatizer */
   private static BioLemmatizer bioLemmatizer = new BioLemmatizer();
   
-  
+  /**
+   * constructor to initialize class fields of LemmaConverter
+   */
   public LemmaConverter () {
-    lemmaMap = new HashMap<String, String>();
+      lemmaLookupMap = new HashMap<String, String>();
   }
   
-/**
-    Modify an {@code BioCAnnotation}.
- */
+  /**
+   * read from the BioC annotation each token and its POS tag,
+   * generate corresponding lemma, and insert the lemma
+   * into the corresponding annotation following BioC format
+   */
+  @Override
   public BioCAnnotation getAnnotation(BioCAnnotation in) {
-    BioCAnnotation out = new BioCAnnotation();
-    out.setID(in.getID());
-    out.setInfons(in.getInfons());
-    out.setText(in.getText());
-    out.setLocations(in.getLocations());
-    String pos = out.getInfon("POS");
-    String token = out.getText();
-    String lemma = generateLemma(token, pos); 
-    out.putInfon("lemma", lemma);
-    return out;
+      BioCAnnotation out = new BioCAnnotation();
+      out.setID(in.getID());
+      out.setInfons(in.getInfons());
+      out.setText(in.getText());
+      out.setLocations(in.getLocations());
+      String pos = out.getInfon("POS");
+      String token = out.getText();
+      String lemma = generateLemma(token, pos); 
+      out.putInfon("lemma", lemma);
+      return out;
   }
  
- /**
-  * Generate lemma for the input String with POS
-  */
- private String generateLemma(String token, String pos) {
-   String lemma;
-   if(lemmaMap.containsKey(token + " " + pos)) {
-      return lemmaMap.get(token + " " + pos);
-   }
-   else {
-     lemma = bioLemmatizer.lemmatizeByLexiconAndRules(token, pos).lemmasToString();
-     lemmaMap.put((token + " " + pos), lemma );
-   } 
-   return lemma;
- }
- 
- 
+  /**
+   * Generate lemma for the input String with POS tag
+   * using the BioLemmatizer, and populate the lemma
+   * lookup map
+   */
+  private String generateLemma(String token, String pos) {
+      String lemma;
+      if(lemmaLookupMap.containsKey(token + " " + pos)) {
+         return lemmaLookupMap.get(token + " " + pos);
+      }
+
+      lemma = bioLemmatizer.lemmatizeByLexiconAndRules(token, pos).lemmasToString();
+      lemmaLookupMap.put((token + " " + pos), lemma );
+
+      return lemma;
+  }
+
 }
