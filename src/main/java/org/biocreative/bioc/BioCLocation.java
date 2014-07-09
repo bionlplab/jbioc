a7023145f0c341fecaf74a5dd934597b16206d4a
Yifan <ypeng@cis.udel.edu>
2014-07-08 23:21:35 -0700

    add hashcode, equal and test to bioclocation

--- src/main/java/org/biocreative/bioc/BioCLocation.java
+++ src/main/java/org/biocreative/bioc/BioCLocation.java
@@ -1,5 +1,10 @@
 package org.biocreative.bioc;
 
+import org.apache.commons.lang3.Validate;
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+
 /**
  * The connection to the original text can be made through the {@code offset},
  * {@code length}, and possibly the {@code text} fields.
@@ -25,8 +30,8 @@ public class BioCLocation {
   }
 
   public BioCLocation(int offset, int length) {
-    this.offset = offset;
-    this.length = length;
+    setOffset(offset);
+    setLength(length);
   }
 
   public int getLength() {
@@ -38,15 +43,42 @@ public class BioCLocation {
   }
 
   public void setLength(int length) {
+    Validate.isTrue(length > 0, "length has to be greater than 0");
     this.length = length;
   }
 
   public void setOffset(int offset) {
+    Validate.isTrue(offset > 0, "offset has to be greater than 0");
     this.offset = offset;
+  }
+  
+  @Override
+  public int hashCode() {
+    return new HashCodeBuilder()
+        .append(offset)
+        .append(length)
+        .toHashCode();
+  }
+
+  public boolean equals(Object obj) {
+    if (obj == this) {
+      return true;
+    }
+    if (obj == null || obj.getClass() != getClass()) {
+      return false;
+    }
+    BioCLocation rhs = (BioCLocation) obj;
+    return new EqualsBuilder()
+        .append(offset, rhs.offset)
+        .append(length, rhs.length)
+        .isEquals();
   }
 
   @Override
   public String toString() {
-    return offset + ":" + length;
+    return new ToStringBuilder(this).
+        append("offset", offset).
+        append("length", length).
+        toString();
   }
 }
---
+++ src/test/java/org/biocreative/bioc/BioCLocationTest.java
@@ -0,0 +1,42 @@
+package org.biocreative.bioc;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+public class BioCLocationTest {
+
+  private static final int LENGTH = 1;
+  private static final int OFFSET = 2;
+
+  private static final BioCLocation BASE = new BioCLocation(OFFSET, LENGTH);
+  private static final BioCLocation BASE_COPY = new BioCLocation(OFFSET, LENGTH);
+
+  @Rule
+  public ExpectedException thrown = ExpectedException.none();
+
+  @Test
+  public void test_equals() {
+    assertEquals(BASE, BASE_COPY);
+  }
+
+  @Test
+  public void test_allFields() {
+    assertEquals(BASE.getLength(), LENGTH);
+    assertEquals(BASE.getOffset(), OFFSET);
+  }
+
+  @Test
+  public void test_negLength() {
+    thrown.expect(IllegalArgumentException.class);
+    new BioCLocation(OFFSET, -1);
+  }
+  
+  @Test
+  public void test_negOffset() {
+    thrown.expect(IllegalArgumentException.class);
+    new BioCLocation(-1, LENGTH);
+  }
+}
