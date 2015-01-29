f959f04caefe4ae156b3cf01edbe01ccfd6ebfa7
Yifan Peng <yfpeng@udel.edu>
2015-01-29 18:25:25 -0500

    comments in io package

--- src/main/java/com/pengyifan/bioc/io/package-info.java
+++ src/main/java/com/pengyifan/bioc/io/package-info.java
@@ -1,22 +1,6 @@
 /**
- * This package defines utility classes for working with readers and writers on
- * BioC files. The most commonly used classes are described here:
- * 
- * <p>
- * <b>BioCCollectionReader</b> reads the entire BioC file into one collection.
- * 
- * <p>
- * <b>BioCDocumentReader</b> sequentially reads the BioC file into document
- * every time the method
- * {@link com.pengyifan.bioc.io.BioCDocumentReader#readDocument} is called.
- * 
- * <p>
- * <b>BioCCollectionWriter</b> writes the
- * {@link com.pengyifan.bioc.BioCCollection} into a file.
- * 
- * <p>
- * <b>BioCDocumentWriter</b> sequentially writes
- * {@link com.pengyifan.bioc.BioCDocument}s into the file.
+ * Defines utility classes for working with readers and writers on
+ * BioC files.
  */
 package com.pengyifan.bioc.io;
 
--- src/main/java/com/pengyifan/bioc/util/BioCDocumentIterator.java
+++ src/main/java/com/pengyifan/bioc/util/BioCDocumentIterator.java
@@ -7,27 +7,58 @@ import com.google.common.collect.Iterators;
 import com.pengyifan.bioc.BioCCollection;
 import com.pengyifan.bioc.BioCDocument;
 
+/**
+ * A document iterator over a collection
+ * 
+ * @since 1.0.0
+ * @see BioCPassageIterator
+ * @see BioCSentenceIterator
+ * @author Yifan Peng
+ */
 public class BioCDocumentIterator implements Iterator<BioCDocument> {
 
   private Iterator<BioCDocument> documentItr;
-  
+
+  /**
+   * Creates an empty iterator.
+   */
   public BioCDocumentIterator() {
     documentItr = Collections.emptyIterator();
   }
-  
+
+  /**
+   * Creates an iterator given the BioC collection.
+   * 
+   * @param collection the BioC collection
+   */
   public BioCDocumentIterator(BioCCollection collection) {
     documentItr = collection.getDocuments().iterator();
   }
-  
+
+  /**
+   * an iterator containing only document.
+   * 
+   * @param document the BioC document
+   */
   public BioCDocumentIterator(BioCDocument document) {
     documentItr = Iterators.singletonIterator(document);
   }
 
+  /**
+   * Returns true if the iteration has more documents.
+   * 
+   * @return if the iteration has more documents
+   */
   @Override
   public boolean hasNext() {
     return documentItr.hasNext();
   }
 
+  /**
+   * Returns the next document in the iteration.
+   * 
+   * @return the next document in the iteration
+   */
   @Override
   public BioCDocument next() {
     return documentItr.next();
