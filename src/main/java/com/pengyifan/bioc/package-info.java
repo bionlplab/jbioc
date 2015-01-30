6bbb87fc79b391dda32f2ef72a1e57728c1214f3
Yifan Peng <yfpeng@udel.edu>
2015-01-29 20:37:57 -0500

    an error

--- src/main/java/com/pengyifan/bioc/package-info.java
+++ src/main/java/com/pengyifan/bioc/package-info.java
@@ -8,7 +8,8 @@
  * XML files, the data can be transferred from one program to another
  * independent of language or research group.
  * <p>
- * All BioC XML files, or collections, begin with a {@link BioCCollection}. A
+ * All BioC XML files, or collections, begin with a
+ * {@link com.pengyifan.bioc.BioCCollection}. A
  * {@link com.pengyifan.bioc.BioCCollection} contains a number of
  * {@link com.pengyifan.bioc.BioCDocument}s. Each
  * {@link com.pengyifan.bioc.BioCDocument} consistent of a series of
